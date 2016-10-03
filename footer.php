05bc94d2111291eb327a33e7d8b2a03bdfe05917
szymonsobczak <szymon.sobczak@getbase.com>
2016-10-02 21:11:17 -0700

    cosmetics

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
             <p class="copyright">Copyritght by Anna, photos by Szymon.</p>
         </footer>
 
-    <?php wp_footer(); ?>
+        <?php wp_footer(); ?>
 
-<div class="scroll-back-to-top-wrapper">
-	<span class="scroll-back-to-top-inner">
-					<i class="fa fa-2x fa-angle-up"></i>
-			</span>
-</div>
+        <div class="scroll-back-to-top-wrapper">
+            <span class="scroll-back-to-top-inner">
+                <i class="fa fa-2x fa-angle-up"></i>
+            </span>
+        </div>
 
 
-  </body>
+    </body>
 </html>
