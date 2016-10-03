4bcd8c058932bd51bdb7f58f5cd7a611394e6ae6
szymonsobczak <szymon.sobczak@getbase.com>
2016-10-02 21:10:40 -0700

    404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="text-center fullscreen-msg">
+	<h1>Nie znaleziono strony.</h1>
+	<br>
+	<h3>
+		<a href="<?php bloginfo('home'); ?>"> Kliknij tutaj</a> żeby wrócić na stronę główną.
+		<br><br>Możesz też poszukać strony, która Cię interesuje w menu na górze.
+	</h3>
+</div>
+<?php get_footer(); ?>
