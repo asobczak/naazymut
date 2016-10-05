043e3d974349f71b75d60d0136450b6200f283ba
szymonsobczak <szymon.sobczak@getbase.com>
2016-10-04 21:12:44 -0700

    single column front page

--- front-page.php
+++ front-page.php
@@ -1,69 +1,86 @@
 <?php get_header(); ?>
 
+<div class='container-fluid'>
 
-<?php if (have_posts()) : ?>
-<?php
-  $count = 0;
-  $posts_s = array();
-?>
-<?php while (have_posts()) : the_post(); ?>
-<?php $count++; ?>
+<?php if ( have_posts() ) : ?>
+	<?php
+		$count   = 0;
+		$posts_s = array();
+	?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-        <?php
-            if($count == 1) {
-                $img_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full')[0];
-                $image = "<a href='".get_the_permalink()."'><div class='image-container' style='background-image: url(".$img_url.");'></div></a>";
+	<?php
+		$count ++;
 
-                echo '<div class="big-section">'.$image.'</div>';
-            }
-        ?>
+		if ( $count == 1 ) {
+			$img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0];
+			$image   = "<a href='" . get_the_permalink() . "'><div class='image-container' style='background-image: url(" . $img_url . ");'></div></a>";
 
-<?php
-  $c = get_the_content('', true);
-  $teaser = substr($c, 0, 700);
+			echo '<div class="big-section">' . $image . '</div>';
+		}
 
-  $img_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large')[0];
-  $image = "<a href='".get_the_permalink()."'><div class='image-container' style='background-image: url(".$img_url.");'></div></a>";
-  $content = "
-      <div class='text-container'>
-        <a href='".get_the_permalink()."'><h2>".get_the_title()."</h2></a>
-        <p>{$teaser}</p>
-      </div>
-      <div class='more pull-right'>
-        <a href='".get_the_permalink()."'>".pll__('More...')."</a>
-      </div>";
+		$c      = get_the_content( '', true );
+		$teaser = substr( $c, 0, 700 );
 
-        if($count == 1) {
-            echo "<div class='container-fluid'>
-                <div class='row main-row'>
-                    <div class='col-md-8 col-md-offset-2 col-sm-12'>
+		$img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' )[0];
+		$image   = "<a href='" . get_the_permalink() . "'><div class='image-container' style='background-image: url(" . $img_url . ");'></div></a>";
+		$content = "
+			<div class='text-container'>
+				<a href='" . get_the_permalink() . "'><h2>" . get_the_title() . "</h2></a>
+				<p>{$teaser}</p>
+			</div>
+			<div class='more pull-right'>
+				<a href='" . get_the_permalink() . "'>" . pll__( 'More...' ) . "</a>
+			</div>";
+
+		if ( $count == 1 ) {
+			echo "
+				<div class='row main-row'>
+                    <div class='col-md-7 col-md-offset-2 col-sm-12'>
                         {$content}
                     </div>
+                </div>
+                <!-- container row -->
+                <div class='row'>
+                	<!-- left column -->
+                	<div class='col-md-4 col-md-offset-2 col-sm-12'>
+                ";
+		} else {
+			echo "
+				<div class='row'>
+					<div class='col-md-12 section'>
+                		{$image}{$content}
+            		</div>
                 </div>";
-        }
-        else if($count % 2 == 0) {
-            echo "<div class='col-md-5 col-md-offset-1 col-sm-6 section'>
-                {$image}{$content}
-            </div>";
-        } else {
-            echo "<div class='col-md-5 col-sm-6 section'>
-                {$image}{$content}
-            </div>";
-        }
-?>
+		}
+		?>
 
 <?php endwhile; ?>
 
+<!--left column-->
+</div>
+<div class="col-md-offset-1 col-md-2 col-sm-3">
+	<?php if ( is_active_sidebar( 'frontpage-widget' ) ) : ?>
+		<div id="tertiary" class="widget-area" role="supplementary">
+			<?php dynamic_sidebar( 'frontpage-widget' ); ?>
+		</div>
+	<?php endif; ?>
+</div>
+<!--container row-->
+</div>
+<!--container fluid-->
 </div>
 
 <?php else: ?>
-  <p><?php _e('Brak postów ;('); ?></p>
+	<p><?php _e( 'Brak postów ;(' ); ?></p>
 <?php endif; ?>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
 <hr>
 
-<p class="text-center" style="font-size: 14;margin-top: 20px">Partnerzy: <a title="Katalog-blogow.pl - Katalog Blogów Polskiej Blogosfery" href="http://katalog-blogow.pl/" target="_blank" data-wpel-link="internal">Katalog-Blogow.pl</a></p>
+<p class="text-center" style="font-size: 14em; margin-top: 20px">Partnerzy: <a
+		title="Katalog-blogow.pl - Katalog Blogów Polskiej Blogosfery" href="http://katalog-blogow.pl/" target="_blank"
+		data-wpel-link="internal">Katalog-Blogow.pl</a></p>
 
 <?php get_footer(); ?>
